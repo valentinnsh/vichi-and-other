f8fa1d96791343ac6866c976c43a21efd6ab3d38
valentinnsh <valentinnsh@gmail.com>
2019-05-19 15:01:02 +0300

    initial commit for newtons method. using modules from ../eq_solvation

---
+++ newton/methods.f90
@@ -0,0 +1,13 @@
+!
+! Copyright (c) 2019 V.Shishkin
+!
+
+module newton_methods
+  use my_prec
+  use matrixopr
+  use solve_methods
+  implicit none
+
+
+
+end program
---
+++ newton/tester.f90
@@ -0,0 +1,13 @@
+!
+! Copyright (c) 2019 V.Shishkin
+!
+
+
+program DEFAULT_NAME
+  use my_prec
+  use matrixopr
+  use solve_methods
+  use newton_methods
+  implicit none
+
+end program DEFAULT_NAME
