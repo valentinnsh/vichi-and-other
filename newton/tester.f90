9b32f20709c9199e4c3db8b32ad553165db532cf
valentinnsh <valentinnsh@gmail.com>
2019-05-20 23:04:46 +0300

    add accurancy check

--- newton/tester.f90
+++ newton/tester.f90
@@ -11,17 +11,22 @@ program DEFAULT_NAME
   implicit none
 
   real(mp), dimension(10) :: X
+  real(mp) :: t1,t2 !timers
   integer :: iter_num
   integer(mp) :: id
-
-  call newton_method(X, iter_num)
 
   id = 100
   open(id, file = 'result.dat', action = 'write')
 
+  call cpu_time(t1)
+  call newton_method(X, iter_num)
+  call cpu_time(t2)
+
   write(id,*)'_Classic Newton method_ '
   write(id,'(F16.8)')  X
-  write(id,*) iter_num, ' iterations'
+  write(id,*) iter_num, ' iterations ',
+  write(id,'F16.8', append = 'no')t2-t1
+  write(id,*) 'seconds'
 
   write(id,*) 'check: system(x) = '
   write(id,'(F16.8)')  calc_fun_vector(X)
