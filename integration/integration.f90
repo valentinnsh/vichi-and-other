566e62fd9ac1732b3f7563bcb24c5c566bbcfde5
valentinnsh <valentinnsh@gmail.com>
2019-05-29 21:36:54 +0300

    Correct some function titles and reformed newton-kots section in the code

--- integration/integration.f90
+++ integration/integration.f90
@@ -14,34 +14,6 @@ contains
     res = 0.5_mp*cos(3.0_mp*x)*exp(0.4_mp*x) + 4.0_mp*sin(3.5_mp*x)*exp(-3.0_mp*x) + 3.0_mp*x
   end function f
 
-  ! Calculating function moments !
-  function calc_moments(z0, z1, al, a) result(m)
-    real(mp), dimension(0:2) :: m
-    real(mp) :: z1, z0, al, a
-
-    m(0) = ((z1-a)**(1-al) - (z0-a)**(1-al))/(1-al)
-    m(1) = ((z1-a)**(2-al) - (z0-a)**(2-al))/(2-al) + a*m(0)
-    m(2) = ((z1-a)**(3-al) - (z0-a)**(3-al))/(3-al) + 2*a*m(1) - a*a*m(0)
-  end function calc_moments
-
-
-  ! Calculating quadratura coefficients !
-  function calc_step_cqf(z0,z1,al, a) result(res)
-    real(mp) :: res, z0,z1,al, a
-    real(mp) :: zc ! center of [z0,z1]
-    real(mp), dimension(0:2) :: m
-    real(mp), dimension(1:3) :: Ai
-
-    m = calc_moments(z0, z1, al, a)
-    zc = (z1+z0)/2
-
-    Ai(1) = (m(2) - m(1)*(zc+z1) + m(0)*zc*z1)/(zc-z0)/(z1-z0)
-    Ai(2) = -(m(2)-m(1)*(z1+z0)+m(0)*z1*z0)/(zc-z0)/(z1-zc)
-    Ai(3) = (m(2) - m(1)*(zc+z0) + m(0)*zc*z0)/(z1-zc)/(z1-z0)
-    res = Ai(1)*f(z0) + Ai(2)*f(zc) + Ai(3)*f(z1)
-  end function calc_step_cqf
-
-
   ! Richards checking
   function richardson(s1, s2, s3) result(res)
     real(mp) :: res, s3,s2, s1
@@ -50,21 +22,53 @@ contains
   end function richardson
 
 
+  !---------------------------------------!
+  !          NEWTON - KOTS METHOD         !
+  !---------------------------------------!
+
+
+  ! Calculating function moments !
+  function calc_nk_moments(z0, z1, al, a) result(m)
+    real(mp), dimension(0:2) :: m
+    real(mp) :: z1, z0, al, a
+
+    m(0) = ((z1-a)**(1-al) - (z0-a)**(1-al))/(1-al)
+    m(1) = ((z1-a)**(2-al) - (z0-a)**(2-al))/(2-al) + a*m(0)
+    m(2) = ((z1-a)**(3-al) - (z0-a)**(3-al))/(3-al) + 2*a*m(1) - a*a*m(0)
+  end function calc_nk_moments
+
+
+  ! Calculating quadratura coefficients !
+  function calc_step_newt_kots_cqf(z0,z1,al, a) result(res)
+    real(mp) :: res, z0,z1,al, a
+    real(mp) :: zc ! center of [z0,z1]
+    real(mp), dimension(0:2) :: m
+    real(mp), dimension(1:3) :: Ai
+
+    m = calc_nk_moments(z0, z1, al, a)
+    zc = (z1+z0)/2
+
+    Ai(1) = (m(2) - m(1)*(zc+z1) + m(0)*zc*z1)/(zc-z0)/(z1-z0)
+    Ai(2) = -(m(2)-m(1)*(z1+z0)+m(0)*z1*z0)/(zc-z0)/(z1-zc)
+    Ai(3) = (m(2) - m(1)*(zc+z0) + m(0)*zc*z0)/(z1-zc)/(z1-z0)
+    res = Ai(1)*f(z0) + Ai(2)*f(zc) + Ai(3)*f(z1)
+  end function calc_step_newt_kots_cqf
+
   function newton_kots(a, b, al, l) result(res)
-    real(mp), dimension(1:3) :: moments
+    real(mp), dimension(1:3) :: m
     real(mp) :: b, al, a, s1, s2, s3, l, res, h
     integer(mp) :: k, i, j
 
     h = (b-a)/2
-    s1 = calc_step_cqf(a,b,al,a)
+    s1 = calc_step_newt_kots_cqf(a,b,al,a)
 
     h = h/2
-    s2 = calc_step_cqf(a, a+h, al, a) + calc_step_cqf(a+h,b,al,a)
+    s2 = calc_step_newt_kots_cqf(a, a+h, al, a) + calc_step_newt_kots_cqf(a+h,b,al,a)
 
     h = h/2; i = 0; s3 = 0
     do while((a+h*i) < b)
        i = i + 1
-       s3 = s3 + calc_step_cqf(a+(i-1)*h, a+i*h,al,a)
+       s3 = s3 + calc_step_newt_kots_cqf(a+(i-1)*h, a+i*h,al,a)
     end do
 
     do while(richardson(s1,s2,s3) > eps)
@@ -73,7 +77,7 @@ contains
        h = h/2; i = 0; s3 = 0
        do while((a+h*i) < b)
           i = i + 1
-          s3 = s3 + calc_step_cqf(a+(i-1)*h, a+i*h,al,a)
+          s3 = s3 + calc_step_newt_kots_cqf(a+(i-1)*h, a+i*h,al,a)
        end do
     end do
 
