5df5ce8719bcb4cd9f04ce59d282a1887abc6189
valentinnsh <valentinnsh@gmail.com>
2019-05-30 03:27:35 +0300

    Gauss method.

--- integration/integration.f90
+++ integration/integration.f90
@@ -21,7 +21,24 @@ contains
     res = abs((s3-s2)**2/(2*s2-s3-s1))
   end function richardson
 
+  function solve_third_deg_eq(inp) result(res)
+    real(mp), dimension(1:3) :: inp, res
+    real(mp) :: q, r, a, b, c, phi, pi
 
+    a = inp(3); b = inp(2); c = inp(1)
+
+    pi = acos(-1.0)
+    q = (a**2-3*b)/9
+    r = (2*a**3-9*a*b+27*c)
+    print *, Q**3 - r**2
+    phi = acos(r*q**(-1.5))/3.0
+
+    !print *, phi
+    res(1) = -2*sqrt(q)*cos(phi) - a/3
+    res(2) = -2*sqrt(q)*cos(phi + 2*pi/3) - a/3
+    res(3) = -2*sqrt(q)*cos(phi - 2*pi/3) - a/3
+
+  end function solve_third_deg_eq
   !---------------------------------------!
   !          NEWTON - KOTS METHOD         !
   !---------------------------------------!
@@ -58,7 +75,7 @@ contains
     real(mp) :: b, al, a, s1, s2, s3, l, res, h
     integer(mp) :: k, i, j
 
-    h = (b-a)/2
+    h = (b-a)
     s1 = calc_step_newt_kots_cqf(a,b,al,a)
 
     h = h/2
@@ -92,60 +109,97 @@ contains
   !---------------------------------------!
 
   function calc_gauss_moments(z0, z1, al, a) result(m)
-    real(mp), dimension(0:3) :: m
+    real(mp), dimension(0:5) :: m
     real(mp) :: z1, z0, al, a
 
-    m(0) = (z1-z0)**(1-al)/(1-al)
+    m(0) = (z1-a)**(1-al)/(1-al)
 
-    m(1) = ((z1-a)**(1-al)*(a-z1*(1-al)) - (z0-a)**(1-al)*(a-z0*(1-al)))/(1-al)/(2-al)
+    m(1) = ((z1-a)**(1-al)*(a-z1*(al-1)) - (z0-a)**(1-al)*(a-z0*(al-1)))/(1-al)/(2-al)
 
-    m(2) = (z0-a)**(1-al)*(2*a**2+2*a*z0*(1-al))+z0**2*(1-al)*(2-al)
-    m(2) = (m(2) - (z1-a)**(1-al)*(2*a**2+2*a*z1*(1-al))+z1**2*(1-al)*(2-al))/(al-3)/(al-2)/(al-1)
+    m(2) =         (z0-a)**(1-al)*(2*a**2+2*a*z0*(1-al)+z0**2*(1-al)*(2-al))
+    m(2) = (m(2) - (z1-a)**(1-al)*(2*a**2+2*a*z1*(1-al)+z1**2*(1-al)*(2-al)))/(al-3)/(al-2)/(al-1)
 
-    m(3) = ((z1-a)**(1-al)*(6*a**3-6*a**2*z1*(al-1)+3*a*z1**2*(1-al)*(2-al)+z1**3*(1-al)*(2-al)*(3-al)))
-    m(3) = m(3) - ((z0-a)**(1-al)*(6*a**3-6*a**2*z0*(al-1)+3*a*z0**2*(1-al)*(2-al)+z0**3*(1-al)*(2-al)*(3-al)))
+    m(3) =        (z1-a)**(1-al)*(6*a**3-6*a**2*z1*(al-1)+3*a*z1**2*(1-al)*(2-al)+z1**3*(1-al)*(2-al)*(3-al))
+    m(3) = m(3) - (z0-a)**(1-al)*(6*a**3-6*a**2*z0*(al-1)+3*a*z0**2*(1-al)*(2-al)+z0**3*(1-al)*(2-al)*(3-al))
     m(3) = m(3)/(al-4)/(al-3)/(al-2)/(al-1)
 
+    m(4) = -(((z1-a)**(1-al)*(24*a**4-24*a**3*z1*(al-1)+12*a**2*z1**2*(al**2-3*al+2) &
+         -4*a*z1**3*(z1**3-6*al**2+11*al-6)+z1**4*(al**4-10*al**3+35*al**2-50*al+24)))) &
+         + (((z0-a)**(1-al)*(24*a**4-24*a**3*z0*(al-1)+12*a**2*z0**2*(al**2-3*al+2) &
+         -4*a*z0**3*(z0**3-6*al**2+11*al-6)+z0**4*(al**4-10*al**3+35*al**2-50*al+24))))
+    m(4) = m(4)/(al-5)/(al-4)/(al-3)/(al-2)/(al-1)
+
+    m(5) = (((-a+z1)**(1-al)*(120*a**5-120*a**4*(-1+al)*z1+60*a**3*(2-3*al+al**2)*z1**2-20*a**2*(-6+11*al-6*al**2+al**3)*z1**3 &
+         +5*a*(24-50*al+35*al**2-10*al**3+al**4)*z1**4-(-120 + 274*al - 225*al**2 + 85*al**3 - 15*al**4 + al**5)*z1**5))  &
+         - ((-a+z0)**(1-al)*(120*a**5-120*a**4*(-1+al)*z0+60*a**3*(2-3*al+al**2)*z1**2-20*a**2*(-6+11*al-6*al**2+al**3)*z0**3 &
+         +5*a*(24-50*al+35*al**2-10*al**3+al**4)*z0**4-(-120 + 274*al - 225*al**2 + 85*al**3 - 15*al**4 + al**5)*z0**5)))  &
+         /((-6 + al)/(-5 + al)/(-4 + al)/(-3 + al)/(-2 + al)/(-1 + al))
   end function calc_gauss_moments
 
   function calc_step_gauss_cqf(z0,z1,al, a) result(res)
-    real(mp) :: res, z0,z1,al, a
+    real(mp) :: res, z0,z1, al, a
     real(mp) :: r1, r0 ! коэффициенты узлового многочлена
     real(mp) :: t1, t0
     real(mp) :: zc ! center of [z0,z1]
-    real(mp), dimension(0:3) :: m
-    real(mp), dimension(1:2) :: Ai
+    real(mp), dimension(0:5) :: m
+    real(mp), dimension(1:3) :: Ai, t
+    real(mp), dimension(1:3) :: B, c
+    real(mp), dimension(3,3) :: S, P
+    integer(mp) :: swaps
+    integer :: i
+    S = 0; P = 0;
 
     m = calc_gauss_moments(z0,z1,al,a)
 
-    r0 = (m(2)**2-m(1)*m(3))/(m(1)**2-m(0)*m(2))
-    r1 = (m(0)*m(3)-m(1)*m(2))/(m(1)**2-m(0)*m(2))
+    S(1,1) = m(0); S(1,2) = m(1); S(1,3) = m(2); B(1) = -m(3)
+    S(2,1) = m(1); S(2,2) = m(2); S(2,3) = m(3); B(2) = -m(4)
+    S(3,1) = m(2); S(3,2) = m(3); S(3,3) = m(4); B(3) = -m(5)
 
-    t0 = 0.5*(-r1 - sqrt(r1**2-4*r0))
-    t1 = 0.5*(-r1 + sqrt(r1**2-4*r0))
+    call decompose_LU(S, P, swaps)
+    call solve_eq_sys(S, P, B, c)
+    !print *, c
+    ! посчитаем, используя тригонометрическую формулу виетта
+    t = solve_third_deg_eq(c)
 
-    Ai(1) = (m(1)-m(0)*t1)/(t0-t1)
-    Ai(2) = m(0) - Ai(1)
+    ! solve final system
+    S(1,1) = 1; S(1,2) = 1; S(1,3) = 1; B(1) = m(0)
+    S(2,1) = t(1); S(2,2) = t(2); S(2,3) = t(3); B(2) = m(1)
+    S(3,1) = t(1)**2; S(3,2) = t(2)**2; S(3,3) = t(3)**2; B(3) = m(2)
 
-    res = Ai(1)*f(z0) + Ai(2)*f(z1)
+    call decompose_LU(S, P, swaps)
+    call solve_eq_sys(S, P, B, Ai)
+
+    !r0 = (m(2)**2-m(1)*m(3))/(m(1)**2-m(0)*m(2))
+    !r1 = (m(0)*m(3)-m(1)*m(2))/(m(1)**2-m(0)*m(2))
+    !print *, r0, r1
+
+
+    !t0 = 0.5*(-r1 - sqrt(r1**2-4*r0))
+    !t1 = 0.5*(-r1 + sqrt(r1**2-4*r0))
+    !Ai(1) = (m(1)-m(0)*t1)/(t0-t1)
+    !Ai(2) = m(0) - Ai(1)
+
+    res = Ai(1)*f(z0) + Ai(2)*f(zc) + Ai(3)*f(z1)
   end function calc_step_gauss_cqf
 
   function gauss_integration(a, b, al, l) result(res)
     real(mp) :: b, al, a, s1, s2, s3, l, res, h
     integer(mp) :: k, i, j
 
-    h = (b-a)/2
-    s1 = calc_step_newt_kots_cqf(a,b,al,a)
+    h = (b-a)
+    s1 = calc_step_gauss_cqf(a,b,al,a)
 
     h = h/2
-    s2 = calc_step_newt_kots_cqf(a, a+h, al, a) + calc_step_newt_kots_cqf(a+h,b,al,a)
+    s2 = calc_step_gauss_cqf(a, a+h, al, a) + calc_step_gauss_cqf(a+h,b,al,a)
 
+    print *, calc_step_gauss_cqf(a+h/2, a+ h, al, a)
     h = h/2; i = 0; s3 = 0
     do while((a+h*i) < b)
        i = i + 1
-       s3 = s3 + calc_step_newt_kots_cqf(a+(i-1)*h, a+i*h,al,a)
+       s3 = s3 + calc_step_gauss_cqf(a+(i-1)*h, a+i*h,al,a)
     end do
 
+    print *, s1, s2, s3
     do while(richardson(s1,s2,s3) > eps)
        s1 = s2; s2 = s3;
 
@@ -153,7 +207,10 @@ contains
        do while((a+h*i) < b)
           i = i + 1
           s3 = s3 + calc_step_gauss_cqf(a+(i-1)*h, a+i*h,al,a)
+          !print *, calc_step_gauss_cqf(a+(i-1)*h, a+i*h,al,a)
        end do
+
+       !print *, s3
     end do
 
     res = s3
